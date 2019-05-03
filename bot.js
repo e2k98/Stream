9075a74b2b1bf0499140ac2c6663ff3efde97cfc
e2k98 <47712788+e2k98@users.noreply.github.com>
2019-05-03 22:20:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : e2k ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`NightBott I .help `,"http://twitch.tv/S-F")
+client.user.setGame(`Hii `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
